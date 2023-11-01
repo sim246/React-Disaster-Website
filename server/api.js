ccb96ae181f9ebdbe61c4d3e283d518830511cf2
unknown <sila.ben-khelifa@dawsoncollege.qc.ca>
2023-11-01 18:47:14 -0400

    Add another api endpoint for economy

--- server/api.js
+++ server/api.js
@@ -73,6 +73,35 @@ app.get('/api/v1/:year/natural-disasters/type/:type', async (req, res)=>{
   }
 });
 
+app.get('/api/v1/:year/gdp', async (req, res)=>{ 
+  res.type('json');
+  if (db) {
+    if (isNaN(parseInt(req.params.year)) || req.params.year < 1960 || req.params.year > 2021) {
+      res.status(404).send({status: '404', message: 'Not found:'});
+    }
+  
+    //Getting gdp by year 
+    let gdpData;
+    try {
+      gdpData = await db.readGdp(req.params.year);
+    } catch (error) {
+      res.status(404).send({status: '404', message: 'Not found: ' + error});
+    }
+    
+    //Optionally getting gdp by country too
+    const countryParam = req.query.country;
+    let filteredData = gdpData;
+    if (countryParam) {
+      filteredData = filteredData.filter((economy)=> economy.country === countryParam);
+    }
+    if (!res.headersSent){
+      res.send(filteredData);
+    }
+  } else {
+    res.status(500).send({status: '500', message: 'Database connection not established'});
+  }
+});
+
 app.use(function (req, res, next) {
   res.status(404).send('Sorry cant find that!');
   next();
