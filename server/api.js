004b0acc3e09fcd665f6035dcd873ac481f45b06
unknown <sila.ben-khelifa@dawsoncollege.qc.ca>
2023-11-01 16:08:12 -0400

    Modify get in api for natural disasters by year by country

--- server/api.js
+++ server/api.js
@@ -23,11 +23,16 @@ app.get('/api/v1/:year/natural-disasters/:country', async (req, res) => {
   res.type('json');
   if (db) {
     if (isNaN(parseInt(req.params.year)) || req.params.year < 1960 || req.params.year > 2021) {
-      res.status(404).send({status: '404', message: '404: Not found'});
+      res.status(404).send({status: '404', message: 'Not found:'});
     }
   
     //Getting disasters by year and country from db
-    var disastersData = await db.readDisasters(req.params.year, req.params.country);
+    let disastersData;
+    try {
+      disastersData = await db.readDisasters(req.params.year, req.params.country);
+    } catch (error) {
+      res.status(404).send({status: '404', message: 'Not found: ' + error});
+    }
     
     const typeParam = req.query.type;
     let filteredData = disastersData;
@@ -43,7 +48,7 @@ app.get('/api/v1/:year/natural-disasters/:country', async (req, res) => {
 app.get('api/v1/:year/natural-disasters/:type', async (req, res)=>{
   let year = req.params.year;
   let type = req.params.type;
-  const types = ["Flood", "Storm", "Earthquake", "Epidemic", "Landslide"];
+  const types = ['Flood', 'Storm', 'Earthquake', 'Epidemic', 'Landslide'];
   if (year < 1960 || year > 2021 || types.includes(type)) {
     if (db) {
       res.type('json');
