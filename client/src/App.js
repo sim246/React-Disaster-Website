e2d8a8ad81344471a7df73d5a711db1072e7f7d7
Vasilii Iurev <yuryev.v@Vasiliis-Macbook-Pro.local>
2023-11-08 14:00:40 -0500

    add Map component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import DisplayApi from  './components/DisplayApi.js';
+import Map from './components/Map.js';
 
 function App() {
   return (
     <div className="App">
-      <DisplayApi />
+      <h1>project</h1>
+      {/* <DisplayApi /> */}
+      <Map />
     </div>
   );
 }
---
+++ client/src/components/Map.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup
+} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './Map.css';
+
+
+
+function Map() {
+
+  return (
+    <div id="map-container">
+    
+    
+      <MapContainer 
+        center={[45.5, -73.6]}
+        zoom={4}
+        zoomControl={true}
+        updateWhenZooming={false}
+        updateWhenIdle={true}
+        preferCanvas={true}
+        minZoom={1}
+        maxZoom={16}
+      >
+        <TileLayer
+          attribution='&copy;
+            <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        
+      </MapContainer>
+    </div>
+  );
+}
+
+export default Map;
