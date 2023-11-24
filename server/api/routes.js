a4dfdb6cc4053e25ef2eea3f0a82cdee021218cd
Simona Georgiana Dragomir <simona.dragomir@dawsoncollege.qc.ca>
2023-11-24 15:47:54 -0500

    started single country route

--- server/api/routes.js
+++ server/api/routes.js
@@ -20,6 +20,39 @@ router.get('/api/v1/:year/gdp', getGDPs);
 
 router.get('/api/v1/countries/coordinates', getCountriesCoordinates);
 
+/**
+ * @swagger
+ * /api/v1/countries/:country:
+ *   get:
+ *     summary: Retrieve a list of mongoDB coordinates for a given country
+ *     description: Retrieve a list of coordinates for a given country from MongoDB. Can be used to populate a map.
+ *     parameters:
+ *       - in: path
+ *         name: country
+ *         required: true
+ *         description: Name of a country to retrieve.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A list of countries.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *               id:
+ *                 type: string
+ *                 description: The country ID.
+ *                 example: "6553d288db6e8109af46433e"
+ *               type:
+ *                 type: string
+ *                 description: The type.
+ *                 example: "Feature"
+ *               properties:
+ *                 type: object
+ *                  properties:
+*/
 router.get('/api/v1/countries/:country', getCountry);
 
 /**
@@ -27,7 +60,7 @@ router.get('/api/v1/countries/:country', getCountry);
  * /api/v1/countries:
  *   get:
  *     summary: Retrieve a list of mongoDB countries
- *     description: Retrieve a list of countries from MongoDB. Can be used to populate the map.
+ *     description: Retrieve a list of countries from MongoDB. Can be used to populate a map.
  *     responses:
  *       200:
  *         description: A list of countries.
