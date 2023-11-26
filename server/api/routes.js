de6fe1407c6db6234c4d34d87c2b1dc0ac7bd73f
Simona Georgiana Dragomir <simona.dragomir@dawsoncollege.qc.ca>
2023-11-26 18:15:19 -0500

    finish swagger for path /api/vi/countries/:country

--- server/api/routes.js
+++ server/api/routes.js
@@ -22,36 +22,56 @@ router.get('/api/v1/countries/coordinates', getCountriesCoordinates);
 
 /**
  * @swagger
- * /api/v1/countries/:country:
+ * /api/v1/countries/{country}:
  *   get:
  *     summary: Retrieve a list of mongoDB coordinates for a given country
  *     description: Retrieve a list of coordinates for a given country from MongoDB. Can be used to populate a map.
  *     parameters:
- *       - in: path
- *         name: country
- *         required: true
- *         description: Name of a country to retrieve.
- *         schema:
- *           type: integer
+ *     - name: country
+ *       in: path
+ *       description: 'country'
+ *       required: true
+ *       type: string
  *     responses:
  *       200:
- *         description: A list of countries.
+ *         description: A list of coordinates for a given country
  *         content:
  *           application/json:
  *             schema:
  *               type: object
  *               properties:
- *               id:
- *                 type: string
- *                 description: The country ID.
- *                 example: "6553d288db6e8109af46433e"
- *               type:
- *                 type: string
- *                 description: The type.
- *                 example: "Feature"
- *               properties:
- *                 type: object
- *                  properties:
+ *                 _id:
+ *                   type: string
+ *                   description: The country ID.
+ *                   example: "6553d288db6e8109af46433e"
+ *                 type:
+ *                   type: string
+ *                   description: The type.
+ *                   example: "Feature"
+ *                 properties:
+ *                   type: object
+ *                   properties:
+ *                     ADMIN:
+ *                       type: string
+ *                       example: "Canada"
+ *                     ISO_A3:
+ *                       type: string
+ *                       example: "CAN"
+ *                 geometry:
+ *                   type: object
+ *                   properties:
+ *                     type:
+ *                       type: string
+ *                       example: "MultyPolygon"
+ *                     coordinates:
+ *                       type: array
+ *                       items:
+ *                         type: array
+ *                         items:
+ *                           type: array
+ *                           items:
+ *                             type: float
+ *                             example: [1.22334353, -2.3482758947]
 */
 router.get('/api/v1/countries/:country', getCountry);
 
